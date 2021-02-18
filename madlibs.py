8a46f01743b1a097817e8742cef057122ac3ff3b
Mohammed Alfathi <iizekalfthi@gmail.com>
2021-02-18 15:25:23 +0400

    Modify and add new thigs.

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,9 @@
 """
 This is a madlib project where you can work with string and complete the sentence with the correct world.
 """
+sentence = "I wake up early that morning and looked outside my window. There was a ____ amount of snow! I couldn't believe mye eyes. It looked so ____. I ran downstairs and turned on the television. School was  closed! I ____ly got dressed and hurriied outside. It was ____. The first thing I did was ____ a snowman. He looks like this."
+print(sentence)
+print("After you read the pragraph, see if you can complete it with the correct word.")
 adj = input("Adjective:")
 noun = input("Noun: ")
 adj2 = input("Adjective: ")
