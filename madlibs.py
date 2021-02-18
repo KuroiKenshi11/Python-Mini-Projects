836de646d680ddd9310dc52ac34c44ce0968d795
Mohammed Alfathi <iizekalfthi@gmail.com>
2021-02-18 15:19:17 +0400

    Add a madlibs python file

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+"""
+This is a madlib project where you can work with string and complete the sentence with the correct world.
+"""
+adj = input("Adjective:")
+noun = input("Noun: ")
+adj2 = input("Adjective: ")
+noun2 = input("Noun: ")
+verb = input("Verb: ")
+
+sentence = f"I wake up early that morning and looked outside my window. There was a {adj} amount of snow! I couldn't believe mye eyes. It looked so {noun}. I ran downstairs and turned on the television. School was  closed! I {adj2}ly got dressed and hurriied outside. It was {noun2}. The first thing I did was {verb} a snowman. He looks like this."
+print(sentence)
